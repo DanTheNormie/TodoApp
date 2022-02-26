4f54a6543bb15092a8ef8290ba9c33e90ef0da53
CmdDaniel <cmd.princedaniel@gmail.com>
2022-02-26 10:23:16 +0530

    1. fixed bottomsheet behaviour with recyclerview \n 2. cleaned UI and added functionality for textInputLayouts except reminder

--- app/src/main/java/com/noice/todo_app/ui/fragments/main_fragments/SubtasksRVAdapter.kt
+++ app/src/main/java/com/noice/todo_app/ui/fragments/main_fragments/SubtasksRVAdapter.kt
@@ -5,77 +5,93 @@ import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.firebase.ui.database.FirebaseRecyclerAdapter
-import com.firebase.ui.database.FirebaseRecyclerAdapter_LifecycleAdapter
-import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.noice.todo_app.R
 import com.noice.todo_app.data.model.Subtask
-import com.noice.todo_app.data.model.Task
 import com.noice.todo_app.databinding.SubtasksItemLayoutBinding
 
 class SubtasksRVAdapter(
     val context: Context,
-    val startIconClickListener:(position: Int)->Unit,
-    val endIconClickListener:(position:Int)->Unit,
-    val onDoneClickListener:(position:Int,text:String)->Unit
-): ListAdapter<Subtask, SubtasksRVAdapter.SubtasksViewHolder>(SubtasksDiffCallback()) {
+    val subtasksList: ArrayList<Subtask>
+) : RecyclerView.Adapter<SubtasksRVAdapter.SubtasksViewHolder>() {
 
-    inner class SubtasksViewHolder(val bind: SubtasksItemLayoutBinding):RecyclerView.ViewHolder(bind.root){
+    inner class SubtasksViewHolder(val bind: SubtasksItemLayoutBinding) :
+        RecyclerView.ViewHolder(bind.root) {
 
-        fun bind(position: Int){
-            val subtask = currentList[position]
-            var list = currentList
+
+        fun bind(position: Int) {
+            val subtask = subtasksList[position]
+
 
 
 
             bind.subtasksEt.setText(subtask.title)
 
-
-            if (subtask.isCompleted){
-                bind.subtasksTil.startIconDrawable = ContextCompat.getDrawable(context,R.drawable.ic_baseline_checked_circle_24)
-                bind.subtasksEt.paintFlags = bind.subtasksEt.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-            }else{
-                bind.subtasksTil.startIconDrawable = ContextCompat.getDrawable(context,R.drawable.ic_baseline_unchecked_circle_24)
-                bind.subtasksEt.paintFlags = bind.subtasksEt.paintFlags or -Paint.STRIKE_THRU_TEXT_FLAG
+            if (subtask.isCompleted) {
+                bind.subtasksTil.startIconDrawable =
+                    ContextCompat.getDrawable(context, R.drawable.ic_baseline_checked_circle_24)
+                bind.subtasksEt.paintFlags =
+                    bind.subtasksEt.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            } else {
+                bind.subtasksTil.startIconDrawable =
+                    ContextCompat.getDrawable(context, R.drawable.ic_baseline_unchecked_circle_24)
+                bind.subtasksEt.paintFlags = 0x00
             }
+
 
             bind.subtasksTil.setStartIconOnClickListener {
-                startIconClickListener.invoke(position)
+                subtasksList[position].isCompleted = !subtask.isCompleted
+                notifyItemChanged(position)
             }
+
             bind.subtasksTil.setEndIconOnClickListener {
-                endIconClickListener.invoke(position)
+
+                subtasksList.removeAt(position)
+                notifyItemRemoved(position)
+                notifyItemRangeChanged(position, subtasksList.size)
             }
             bind.subtasksEt.setOnEditorActionListener { v, actionId, event ->
                 if (actionId == EditorInfo.IME_ACTION_DONE){
-                    onDoneClickListener.invoke(position,bind.subtasksEt.text.toString())
+                    Toast.makeText(
+                        context,
+                        "text updated to ${bind.subtasksEt.text.toString()}",
+                        Toast.LENGTH_SHORT
+                    ).show()
                     return@setOnEditorActionListener true
                 }
 
                 false
             }
+
         }
+
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubtasksRVAdapter.SubtasksViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.subtasks_item_layout,parent,false)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): SubtasksRVAdapter.SubtasksViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.subtasks_item_layout, parent, false)
         val bind = SubtasksItemLayoutBinding.bind(view)
 
-       return SubtasksViewHolder(bind)
+        return SubtasksViewHolder(bind)
     }
 
     override fun onBindViewHolder(holder: SubtasksRVAdapter.SubtasksViewHolder, position: Int) {
         holder.bind(position)
     }
+
+    override fun getItemCount(): Int {
+        return subtasksList.size
+    }
 }
 
-class SubtasksDiffCallback():DiffUtil.ItemCallback<Subtask>(){
+class SubtasksDiffCallback : DiffUtil.ItemCallback<Subtask>() {
     override fun areItemsTheSame(oldItem: Subtask, newItem: Subtask): Boolean {
         return oldItem.id == newItem.id
     }
